24c8b46b7b3fbbc36521b5a854f3686e791827d7
TechnoIndian <133761719+TechnoIndian@users.noreply.github.com>
2024-06-08 18:11:40 +0530

    java.sh

--- files/java.sh
+++ files/java.sh
@@ -1,1 +1,1 @@
-pkg install wget && wget https://github.com/h4ck3r0/Java-termux/releases/download/javafix.sh/javafix.sh && bash javafix.sh
+pkg install wget && wget https://github.com/TechnoIndian/Java-Termux/releases/download/javafix.sh/javafix.sh && bash javafix.sh
