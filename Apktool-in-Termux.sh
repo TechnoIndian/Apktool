77901eb11f4bff34e2e4e46ed8ebd392c7776683
Techno India <133761719+TechnoIndian@users.noreply.github.com>
2024-06-24 13:05:03 +0530

    Delete Apktool-in-Termux.sh

--- Apktool-in-Termux.sh
+++
@@ -1,8 +0,0 @@
-apt-get update -y
-apt-get upgrade -y
-pkg install git -y
-git clone https://github.com/TechnoIndian/Apktool
-cd Apktool && chmod +x *
-bash setup.sh
-cd $Home && cd Apktool
-bash apktool.sh
