8531dda3b7bee35f904493ccf329fc8a3a16d38b
Techno India <133761719+TechnoIndian@users.noreply.github.com>
2024-06-24 12:50:09 +0530

    Update Apktool-in-Termux.sh

--- Apktool-in-Termux.sh
+++ Apktool-in-Termux.sh
@@ -2,4 +2,4 @@ apt-get update -y
 apt-get upgrade -y
 pkg install git -y
 git clone https://github.com/TechnoIndian/Apktool
-cd Apktool && chmod +x * && bash setup.sh && bash apktool.sh
+cd Apktool && chmod +x * && bash setup.sh && cd $Home && cd Apktool && bash apktool.sh
